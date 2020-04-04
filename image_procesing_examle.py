c5313de03a3cf186c6763b0c890b412ffc4b5cb3
zeinsalahppu <zeinsalah@gmail.com>
2020-04-04 21:15:56 +0300

    pointwise_operation_pillow

--- image_procesing_examle.py
+++ image_procesing_examle.py
@@ -4,16 +4,24 @@ Image Processing Example
 """
 
 from PIL import Image
-img = Image.open("d:/develope/ocvtest/f1.JPG")
 
-print(img.format)
-print(img.size)
-print(img.mode)
+img = Image.open("d:/develope/ocvtest/f2_new.JPG")
 
-img.show()
+size = img.size
+print(size)
 
-small_img = img.crop((0, 0, 300, 200))
+neg_img1 = Image.new('RGB', size)
 
-small_img.show()
+for i in range(size[0]):
+    for j in range(size[1]):
+        in_pixel = img.getpixel((i, j))
+        out_pixel = (255 - in_pixel[0], 255 - in_pixel[1], 255 - in_pixel[2])
+        neg_img1.putpixel((i, j), out_pixel)
 
-small_img.save("d:/develope/ocvtest/f1_new.JPG")
+neg_img2 = img.point(lambda i: 255-i)
+
+neg_img1.show()
+neg_img2.show()
+
+neg_img1.save("d:/develope/ocvtest/f1_new_neg1.JPG")
+neg_img2.save("d:/develope/ocvtest/f1_new_neg2.JPG")
