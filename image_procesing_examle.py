14cf6c188cf89dc3b48363994702db16dc1b3417
zeinsalahppu <zeinsalah@gmail.com>
2020-04-06 00:00:33 +0300

    water_coloring_with_added_edges

--- image_procesing_examle.py
+++ image_procesing_examle.py
@@ -1,19 +1,63 @@
 """
-Image Processing Example
+Image Processing Example with openCV
+Water Coloring
 
 """
 
-from PIL import Image
-img = Image.open("d:/develope/ocvtest/f1.JPG")
+import cv2 as cv
+import numpy as np
 
-print(img.format)
-print(img.size)
-print(img.mode)
 
-img.show()
+def reduceValHSV(val, factor):
+    return factor * (val // factor)
 
-small_img = img.crop((0, 0, 300, 200))
 
-small_img.show()
+def reduceColorsHSV(img, factor):
+    hsv_img = cv.cvtColor(img, cv.COLOR_BGR2HSV)
+    h, s, v = cv.split(hsv_img)
 
-small_img.save("d:/develope/ocvtest/f1_new.JPG")
+    size = img.shape
+    for i in range(size[0]):
+        for j in range(size[1]):
+            h[i, j] = reduceValHSV(h[i, j], factor)
+            s[i, j] = int(s[i, j] * 0.75)
+            v[i, j] = min(int(v[i, j] * 1.5), 255)
+
+    hsv_img = cv.merge([h, s, v])
+    rgb_img = cv.cvtColor(hsv_img, cv.COLOR_HSV2BGR)
+    return rgb_img
+
+
+def generate_edges(img):
+    grey_img = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+    return cv.Canny(grey_img, 10, 120)
+
+
+# ------------------------------------
+img = cv.imread('d:/develope/ocvtest/f2_new.JPG', cv.IMREAD_COLOR)
+cv.imshow('input', img)
+
+size = img.shape
+print(size)
+
+kernel = np.ones((5, 5), np.float32)/25
+smoothed_img = cv.filter2D(img, -1, kernel)
+
+edge_img = generate_edges(smoothed_img)
+# cv.imshow('edge image', edge_img)
+
+reduced_color_img = reduceColorsHSV(smoothed_img, 10);
+# cv.imshow('reduced colors', reduced_color_img)
+
+for i in range(size[0]):
+    for j in range(size[1]):
+        if edge_img[i, j] != 0:
+            reduced_color_img[i, j][0] = 0
+            reduced_color_img[i, j][1] = 0
+            reduced_color_img[i, j][2] = 0
+
+cv.imshow('water color image', reduced_color_img)
+
+
+cv.waitKey(0)
+
